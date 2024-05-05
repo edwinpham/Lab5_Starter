a06d0d7c3ba3bb179c3947b09b8321e210981735
Azpi <jesusazpitartedoesart@gmail.com>
2024-05-04 18:16:18 -0700

    attemp3

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -9,7 +9,9 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
-
+ 
 test('adds 1 + 2 should not equal 4', () => {
   expect(sum(1,2)).toBe(4);
 });
+
+//comment
